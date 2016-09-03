3a88253f7a90e2b386b76aa70a03c5b2107f070f
d3jii <d3jii@ultroninc.com>
2016-09-03 23:31:55 +0100

    Init

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Content Script Manager
+
+Simple chrome extension to keep track of the scripts I run (a lot) in the console. 
+Ease of re-use and execution.
---
+++ popup.js
@@ -0,0 +1,136 @@
+var cache_snippets = [];
+
+function getCodeSnippets(callback){
+	chrome.storage.local.get('snippets', function(store){
+
+		if(!store['snippets']){
+			store['snippets'] = [];
+			chrome.storage.local.set({'snippets':store['snippets']});
+		}
+
+		callback(store['snippets']);
+	});
+}
+
+function deleteCodeSnippet(index){
+	getCodeSnippets( function(snippets){
+		if(typeof index != 'undefined' && index != ''){
+			snippets.splice(index, 1);
+			chrome.storage.local.set({'snippets':snippets});
+			doGetSnippets(0);
+			displaySnippet('abc');
+		}
+	})
+}
+
+
+function saveCodeSnippet(snippet, callback, optional_snippet_index){
+	getCodeSnippets( function(snippets){
+		if(typeof optional_snippet_index == 'undefined' || optional_snippet_index == ""){
+			snippets.push(snippet);
+			optional_snippet_index = snippets.length - 1;
+		}
+		else
+			snippets[optional_snippet_index] = snippet;
+
+		chrome.storage.local.set({'snippets':snippets}, function(){ callback(optional_snippet_index); });
+	});
+}
+
+function renderSnippets(container, snippets) {
+	$(container).html('');
+	$(container).append( '<option value="'+""+'">' + " new snippet " + '</option>' );
+	snippets.forEach(  function(s, i){
+		$(container).append( '<option value="'+i+'">' + s.title + '</option>' );
+	});
+}
+
+function displaySnippet(snippet_index){
+	var snippet = cache_snippets[snippet_index];
+
+	if(snippet){
+		$('#code').html(snippet.code);
+		$('#code').val(snippet.code);
+		$('#snippet_title').val(snippet.title);
+		$('#snippet_index').val(snippet_index);
+	}
+	else{
+		$('#code').html("");
+		$('#code').val("");
+		$('#snippet_title').val("");
+		$('#snippet_index').val("");
+	}
+	
+}
+
+function doGetSnippets(is_init){
+	getCodeSnippets(  function(snippets){
+		//alert(snippets.length);
+		cache_snippets = snippets;
+		renderSnippets('#snippets', snippets);
+		if(typeof is_init != 'undefined' && is_init != ''){
+			displaySnippet(is_init);
+			$("#snippets").val(is_init);
+		}
+	});
+}
+
+
+
+$(document).ready( function (e){
+
+	doGetSnippets();
+
+	$(".run-script").click(  function(e){
+		chrome.tabs.query({active:true}, function (tabs){
+			
+			if (tabs && tabs.length){
+				var tab  = tabs[0];
+				var code = $('#code').val(); 
+				if(code && code != "")
+				chrome.tabs.executeScript(tab.id, {code:code});
+			}
+
+		});
+	});
+
+
+	$("#snippets").change(  function(e){ 
+		if(cache_snippets && cache_snippets.length){
+			displaySnippet($(this).val());
+			//var snippet = cache_snippets[$(this).val()];
+			//$('#code').html(snippet.code);
+			//$('#snippet_title').val(snippet.title);
+		}
+	});
+
+	$(".delete").click( function(e){
+
+		//var codeSnippets = [];
+		var snippet_index = $("#snippet_index").val();  
+		if(snippet_index != ""){
+
+			deleteCodeSnippet(snippet_index);	
+		}
+		
+
+
+	});
+
+	$(".save").click( function(e){
+
+		//var codeSnippets = [];
+		var snippet_index = $("#snippet_index").val();
+		var code = $('#code').val(); 
+		var snippet_title = $('#snippet_title').val() || 'Untitled - ' + new Date().toString();
+		if(code && code != ""){
+
+			saveCodeSnippet({code:code,title:snippet_title}, doGetSnippets, snippet_index);	
+		}
+		
+
+
+	});
+
+
+});
